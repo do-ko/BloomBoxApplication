710975708faf82bae2815b4ae72bc08d0dfadf0e
do-ko <doko@onet.eu>
2024-01-09 00:06:00 +0100

    code cleanup

--- BloomBoxApp/src/screens/PlantScreen.js
+++ BloomBoxApp/src/screens/PlantScreen.js
@@ -68,6 +68,9 @@ const PlantScreen = ({route, navigation}) => {
         getAllLocationForUser();
         // setTasks(remainders.filter(rem => rem.plantId === plant.plantId));
         formatMarkedDots();
+        let today = new Date();
+        let todayDateString = `${today.getFullYear()}-${today.getMonth()+1 < 10 ? "0" + (today.getMonth()+1) : today.getMonth()+1}-${today.getDate() < 10 ? "0" + today.getDate() : today.getDate()}`
+        getRemaindersForToday(todayDateString);
     }, [])
 
 
@@ -132,7 +135,7 @@ const PlantScreen = ({route, navigation}) => {
         });
         // console.log(remToday);
         setTasksForToday(remToday);
-        return remToday;
+        // return remToday;
     }
 
     const plantChanged = (plant)=>{
@@ -246,112 +249,11 @@ const PlantScreen = ({route, navigation}) => {
                         <Text style={styles.Title}>Calendar</Text>
                     </View>
                     <View>
-                        {/*<Agenda*/}
-                        {/*    // The list of items that have to be displayed in agenda. If you want to render item as empty date*/}
-                        {/*    // the value of date key has to be an empty array []. If there exists no value for date key it is*/}
-                        {/*    // considered that the date in question is not yet loaded*/}
-                        {/*    items={{*/}
-                        {/*        '2024-01-08': [{name: 'item 1 - any js object'}],*/}
-                        {/*        '2024-01-09': [{name: 'item 2 - any js object', height: 80}],*/}
-                        {/*        '2024-01-11': [],*/}
-                        {/*        '2024-01-12': [{name: 'item 3 - any js object'}, {name: 'any js object'}]*/}
-                        {/*    }}*/}
-                        {/*    // Callback that gets called when items for a certain month should be loaded (month became visible)*/}
-                        {/*    loadItemsForMonth={month => {*/}
-                        {/*        console.log('trigger items loading');*/}
-                        {/*    }}*/}
-                        {/*    // Callback that fires when the calendar is opened or closed*/}
-                        {/*    onCalendarToggled={calendarOpened => {*/}
-                        {/*        console.log(calendarOpened);*/}
-                        {/*    }}*/}
-                        {/*    // Callback that gets called on day press*/}
-                        {/*    onDayPress={day => {*/}
-                        {/*        console.log('day pressed');*/}
-                        {/*    }}*/}
-                        {/*    // Callback that gets called when day changes while scrolling agenda list*/}
-                        {/*    onDayChange={day => {*/}
-                        {/*        console.log('day changed');*/}
-                        {/*    }}*/}
-                        {/*    // Initially selected day*/}
-                        {/*    selected={'2024-01-07'}*/}
-                        {/*    // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined*/}
-                        {/*    // minDate={'2012-05-10'}*/}
-                        {/*    // // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined*/}
-                        {/*    // maxDate={'2012-05-30'}*/}
-                        {/*    // Max amount of months allowed to scroll to the past. Default = 50*/}
-                        {/*    pastScrollRange={50}*/}
-                        {/*    // Max amount of months allowed to scroll to the future. Default = 50*/}
-                        {/*    futureScrollRange={50}*/}
-                        {/*    // Specify how each item should be rendered in agenda*/}
-                        {/*    // renderItem={(item, firstItemInDay) => {*/}
-                        {/*    //     return <View><Text>ITEM</Text></View>;*/}
-                        {/*    // }}*/}
-                        {/*    // Specify how each date should be rendered. day can be undefined if the item is not first in that day*/}
-                        {/*    // renderDay={(day, item) => {*/}
-                        {/*    //     return <View />;*/}
-                        {/*    // }}*/}
-                        {/*    // Specify how empty date content with no items should be rendered*/}
-                        {/*    // renderEmptyDate={() => {*/}
-                        {/*    //     return <View />;*/}
-                        {/*    // }}*/}
-                        {/*    // // Specify how agenda knob should look like*/}
-                        {/*    // renderKnob={() => {*/}
-                        {/*    //     return <View />;*/}
-                        {/*    // }}*/}
-                        {/*    // Specify what should be rendered instead of ActivityIndicator*/}
-                        {/*    // renderEmptyData={() => {*/}
-                        {/*    //     return <View />;*/}
-                        {/*    // }}*/}
-                        {/*    // Specify your item comparison function for increased performance*/}
-                        {/*    rowHasChanged={(r1, r2) => {*/}
-                        {/*        return r1.text !== r2.text;*/}
-                        {/*    }}*/}
-                        {/*    // Hide knob button. Default = false*/}
-                        {/*    // hideKnob={true}*/}
-                        {/*    // When `true` and `hideKnob` prop is `false`, the knob will always be visible and the user will be able to drag the knob up and close the calendar. Default = false*/}
-                        {/*    // showClosingKnob={false}*/}
-                        {/*    // By default, agenda dates are marked if they have at least one item, but you can override this if needed*/}
-                        {/*    markedDates={{*/}
-                        {/*        '2024-01-08': {selected: true, marked: true},*/}
-                        {/*        '2024-01-09': {marked: true},*/}
-                        {/*        '2024-01-10': {disabled: true}*/}
-                        {/*    }}*/}
-                        {/*    // If disabledByDefault={true} dates flagged as not disabled will be enabled. Default = false*/}
-                        {/*    // disabledByDefault={true}*/}
-                        {/*    // If provided, a standard RefreshControl will be added for "Pull to Refresh" functionality. Make sure to also set the refreshing prop correctly*/}
-                        {/*    onRefresh={() => console.log('refreshing...')}*/}
-                        {/*    // Set this true while waiting for new data from a refresh*/}
-                        {/*    refreshing={false}*/}
-                        {/*    // Add a custom RefreshControl component, used to provide pull-to-refresh functionality for the ScrollView*/}
-                        {/*    refreshControl={null}*/}
-                        {/*    // Agenda theme*/}
-                        {/*    // theme={{*/}
-                        {/*    //     ...calendarTheme,*/}
-                        {/*    //     agendaDayTextColor: 'yellow',*/}
-                        {/*    //     agendaDayNumColor: 'green',*/}
-                        {/*    //     agendaTodayColor: 'red',*/}
-                        {/*    //     agendaKnobColor: 'blue'*/}
-                        {/*    // }}*/}
-                        {/*    // Agenda container style*/}
-                        {/*    style={{}}*/}
-                        {/*/>*/}
-                        {/*<Agenda*/}
-                        {/*    items={items}*/}
-                        {/*    loadItemsForMonth={loadItems}*/}
-                        {/*    selected={'2017-05-16'}*/}
-                        {/*    renderItem={renderItem}*/}
-                        {/*/>*/}
                         <Calendar
                             // Handler which gets executed on day press. Default = undefined
                             onDayPress={day => {
-                                // console.log('selected day', day);
-                                // console.log("MARKED DATES");
-                                // console.log(markedDatesOnCal);
-                                console.log("=============")
                                 let tempArrayWithDates = markedDatesOnCal;
-                                console.log("CURRENT DAY: ", currentDay)
 
-                                // FIX THIS SELECTION:
                                 // 1. handle old selected:
                                 if (currentDay !== todayDate){
                                 //     if old selected is not today's date
@@ -386,84 +288,13 @@ const PlantScreen = ({route, navigation}) => {
                                             tempArrayWithDates[day.dateString] = {selected: true, selectedColor: "#5B6E4E", marked: true, dotColor: "#fff"};
                                         }
                                 }
-                                // if (tempArrayWithDates[day.dateString].marked === undefined){
-                                // //     if new selected is not marked
-                                //         tempArrayWithDates[day.dateString] = {selected: true, selectedColor: "#000"};
-                                // } else {
-                                //     // if new selected is marked
-                                //     tempArrayWithDates[day.dateString] = {selected: true, selectedColor: "#000", marked: true, dotColor: "#fff"};
-                                // }
-
-
-
-                                // if (tempArrayWithDates[currentDay].marked === undefined) {
-                                //     tempArrayWithDates[currentDay] = {selected: false, selectedColor: "#000"};
-                                //
-                                //     if (tempArrayWithDates[day.dateString] === undefined) {
-                                //         tempArrayWithDates[day.dateString] = {selected: true, selectedColor: "#000"};
-                                //     } else {
-                                //         tempArrayWithDates[day.dateString] = {marked: true, dotColor: "#fff", selected: true, selectedColor: "#000"};
-                                //     }
-                                //
-                                // } else {
-                                //     tempArrayWithDates[currentDay] = {marked: true, dotColor: "#bbb", selected: false, selectedColor: "#000"};
-                                //     if (tempArrayWithDates[day.dateString] === undefined) {
-                                //         tempArrayWithDates[day.dateString] = {selected: true, selectedColor: "#000"};
-                                //     } else {
-                                //         tempArrayWithDates[day.dateString] = {marked: true, dotColor: "#fff", selected: true, selectedColor: "#000"};
-                                //     }
-                                // }
-
-
-
-                                // remove selected from current
-                                // console.log(tempArrayWithDates[currentDay]);
-                                // console.log(tempArrayWithDates[currentDay].marked);
-                                // if (tempArrayWithDates[currentDay].marked === undefined) {
-                                // //     NOT A TASK
-                                //     tempArrayWithDates[currentDay].selected = false
-                                // } else {
-                                //     tempArrayWithDates[currentDay].selected = false;
-                                //     tempArrayWithDates[currentDay].marked = true;
-                                // }
-                                // // markedDatesOnCal[currentDay].selected = false
-                                //
-                                // // add selected to new day
-                                // if (tempArrayWithDates[day.dateString] === undefined) {
-                                // //     if today is not on marked list
-                                //     console.log("MARKED DATES UNDEFINED - notTask");
-                                //     // let temp = tempArrayWithDates;
-                                //     // console.log("TEMP BEFORE:");
-                                //     // console.log(temp);
-                                //     tempArrayWithDates[day.dateString] = {selected: true, selectedColor: '#8AA578'};
-                                //     // console.log("TEMP AFTER:");
-                                //     // console.log(temp);
-                                //     // setMarkedDatesOnCal(temp);
-                                //
-                                // } else {
-                                //     console.log("MARKED DATES DEFINED - Task");
-                                //     tempArrayWithDates[day.dateString].selected = true;
-                                //     tempArrayWithDates[day.dateString].selectedColor = '#8AA578';
-                                //     tempArrayWithDates[day.dateString].dotColor = '#FFFFFF';
-                                //
-                                // }
-
-
-
-
 
                                 setMarkedDatesOnCal(tempArrayWithDates);
-
-
 
                                 // change current day
                                 setCurrentDay(day.dateString)
 
-
                                 getRemaindersForToday(day.dateString);
-
-                                // setTasks(testDates["2024-01-08"])
-                                // console.log('tasks', tasks);
 
                             }}
                             // // Handler which gets executed on day long press. Default = undefined
